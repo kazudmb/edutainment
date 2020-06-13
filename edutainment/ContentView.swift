bab526931a4168761cf849acfb86c045a8fcaf86
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-13 11:44:04 +0900

    Initial Commit

---
+++ edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  edutainment
+//
+//  Created by KazukiNakano on 2020/06/13.
+//  Copyright © 2020 kazu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
